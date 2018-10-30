3a1f1264803f384ffbb715d9f2669c06b0b7c804
Developer <mathonsiphumzile@gmail.com>
2018-10-30 12:04:01 +0200

    renamed class

--- app/src/main/java/com/example/developer/ziptown/models/responses/GenericBasePostGetResponse.java
+++ app/src/main/java/com/example/developer/ziptown/models/responses/GenericBasePostGetResponse.java
@@ -2,10 +2,10 @@ package com.example.developer.ziptown.models.responses;
 
 import com.example.developer.ziptown.models.objectModels.Offers;
 
-public class GetAllOffersRequestResponse {
+public class GenericBasePostGetResponse {
     private Offers post;
 
-    public GetAllOffersRequestResponse() {
+    public GenericBasePostGetResponse() {
 
     }
 
