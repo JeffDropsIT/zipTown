a27e29d98f9b97dea32cc1dcb1592d8d14fc4684
Developer <mathonsiphumzile@gmail.com>
2018-11-14 10:48:27 +0200

    added currentUserFragment

--- app/src/main/java/com/devdesign/developer/ziptown/activities/HomeActivity.java
+++ app/src/main/java/com/devdesign/developer/ziptown/activities/HomeActivity.java
@@ -1,8 +1,9 @@
 package com.devdesign.developer.ziptown.activities;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,9 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.WindowManager;
 
 import com.devdesign.developer.ziptown.R;
+import com.devdesign.developer.ziptown.fragments.drawerFragments.CurrentUserFragment;
 import com.jaeger.library.StatusBarUtil;
 
 public class HomeActivity extends AppCompatActivity {
@@ -35,23 +36,31 @@ public class HomeActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         // set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
+
                         switch (menuItem.getItemId()){
                             case R.id.id_profile:
-                                Intent intent = new Intent(HomeActivity.this, CurrentUserActivity.class);
-                                startActivity(intent);
-                                finish();
+                                CurrentUserFragment currentUserFragment = new CurrentUserFragment();
+                                setFragment(currentUserFragment);
                                 break;
                         }
                         // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
+                        // Insert the fragment by replacing any existing fragment
 
+
+                        // For example, swap UI fragments here
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        setTitle(menuItem.getTitle());
+
+                        mDrawerLayout.closeDrawers();
                         return true;
                     }
                 });
 
+    }
+    private void setFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_layout, fragment).commit();
     }
     private void setToolBar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
